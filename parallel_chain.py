8e6c288fa67912a6395b1112402e6f2a76fb3510
tygopal45 <jeegopal835@gmail.com>
2026-04-11 02:08:31 +0530

    modified parallel chains using runnables

--- parallel_chain.py
+++ parallel_chain.py
@@ -5,6 +5,8 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+from langchain_core.runnables import RunnableParallel
+
 
 load_dotenv()
 
@@ -18,29 +20,40 @@ llm2 = HuggingFaceEndpoint(
     task="text-generation"
 )
 
-from langchain_core.runnables import RunnableParallel
-
 model1 = ChatHuggingFace(llm=llm1)
 model2 = ChatHuggingFace(llm=llm2)
+
 
 prompt1 = PromptTemplate(
     template="Generate short and simple notes from the following text \n {text}.",
     input_variables=["text"]
 )
+
 prompt2 = PromptTemplate(
     template="Generate 5 Q&A from the following notes \n {text}.",
     input_variables=["text"]
 )
+
 prompt3 = PromptTemplate(
     template="Merge the following notes and quiz into a single document \n notes -> {notes} and quiz -> {quiz}.",
     input_variables=["notes", "quiz"]
 )
+
 parser = StrOutputParser()
+
+# first we develop parallel chains
+# then handle the merger of both parts
+
+
+
 parallel_chain = RunnableParallel({
     'notes' : prompt1 | model1 | parser,
     'quiz' : prompt2 | model2 | parser
 })
+
+
 merge_chain = prompt3 | model1 | parser
+
 chain = parallel_chain | merge_chain
 
 result = chain.invoke({'text': "Python is a high-level, interpreted programming language known for its simplicity and readability. It was created by Guido van Rossum and first released in 1991. Python supports multiple programming paradigms, including procedural, object-oriented, and functional programming. It has a large standard library and a vibrant ecosystem of third-party packages, making it popular for web development, data analysis, artificial intelligence, scientific computing, and more. Python's syntax emphasizes code readability, which allows developers to express concepts in fewer lines of code compared to other programming languages. It is widely used in various industries and has a strong community that contributes to its growth and development."})
